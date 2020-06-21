26e4d03d4abba2b07141367b29002d3154733bed
raxulbru <ramon.xulvi@epn.edu.ec>
2020-06-20 23:58:42 -0500

    Module 4 - Solution.

---
+++ module4-solution/script.js
@@ -0,0 +1,10 @@
+(function () {
+    for (var ii=0; ii<names.length; ii=ii+1) {
+        //var first = names[ii].charAt(0).toLowerCase();
+        //if(first==='j') {byeSpeaker.speak(names[ii]);} 
+        //else {helloSpeaker.speak(names[ii]);}
+        var first = names[ii].charAt(0);
+        if(first==="j"||first==="J") {byeSpeaker.speak(names[ii]);} 
+        else {helloSpeaker.speak(names[ii]);}
+    }
+})();
