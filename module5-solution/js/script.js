bf088cda4af046c76f7565fbf85d398737f7705e
raxulbru <ramon.xulvi@epn.edu.ec>
2020-06-28 23:43:11 -0500

    Module5-Solution.

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -141,7 +141,7 @@ function buildAndShowHomeHTML (categories) {
       //  "randomCategoryShortName","'" + chosenCategoryShortName.short_name + "'");
       //homeHtml = insertProperty(homeHtml,"randomCategoryShortName",short_name);
       //homeHtmlUrl = insertProperty(homeHtmlUrl,"randomCategoryShortName",homeHtmlToInsertIntoMainPage);
-      insertHtml("#main-content", homeHtmlUrl);
+      insertHtml("#main-content", homeHtml);
       //document.querySelector("#main-content").innerHTML = homeHtml;
       //$ajaxUtils.sendGetRequest(
       //  menuItemsTitleHtml,
